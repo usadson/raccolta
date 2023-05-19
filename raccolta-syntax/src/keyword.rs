65c9ff121c1fe32f701134d2e3a4ab9b2cf115c6
Tristan Gerritsen <code@thewoosh.org>
2023-05-19 12:48:58 +0200

    [syntax] Add `NonReservedWord` keyword type
    This change is the first in a series which migrates the existing Keyword
    enum into one with non-reserved words and reserved-words.
    
    As of now, `Keyword` incorrectly contains a list of both
    non-reserved words and reserved-words. This is a problem, because
    non-reserved words contains a list of words that sometimes have semantic
    meaning, but are otherwise commonly used as table and column names. (For
    example, the non-reserved word `NUMBER`).

--- raccolta-syntax/src/keyword.rs
+++ raccolta-syntax/src/keyword.rs
@@ -792,3 +792,598 @@ pub enum Keyword {
     Zone,
     Zerofill,
 }
+
+/// Non-reserved words (`<non-reserved word>`) are words that are allowed in
+/// some context as identifiers, and others as keywords. This is to avoid
+/// clashing with commonly used column or table names such as `NUMBER`.
+///
+/// SQL-86 didn't have these `<non-reserved words>`, as they were introduced in
+/// SQL-1992.
+///
+/// # About
+/// **Specification:** SQL 1992, SQL 1999
+#[derive(Copy, Clone, Debug, PartialEq, Eq, PartialOrd, Ord)]
+#[derive(AsRefStr, EnumIter, strum::Display)]
+#[non_exhaustive]
+#[strum(serialize_all = "SCREAMING_SNAKE_CASE")]
+pub enum NonReservedWord {
+    /// # About
+    /// **Specification:** SQL 1999
+    Abs,
+
+    /// # About
+    /// **Specification:** SQL 1992
+    Ada,
+
+    /// # About
+    /// **Specification:** SQL 1999
+    Asensitive,
+
+    /// # About
+    /// **Specification:** SQL 1999
+    Assignment,
+
+    /// # About
+    /// **Specification:** SQL 1999
+    Asymmetric,
+
+    /// # About
+    /// **Specification:** SQL 1999
+    Atomic,
+
+    /// # About
+    /// **Specification:** SQL 1999
+    Avg,
+
+
+    /// # About
+    /// **Specification:** SQL 1999
+    Between,
+
+    /// # About
+    /// **Specification:** SQL 1999
+    BitLength,
+
+    /// # About
+    /// **Specification:** SQL 1999
+    Bitvar,
+
+
+    /// # About
+    /// **Specification:** SQL 1992
+    C,
+
+    /// # About
+    /// **Specification:** SQL 1999
+    Called,
+
+    /// # About
+    /// **Specification:** SQL 1999
+    Cardinality,
+
+    /// # About
+    /// **Specification:** SQL 1992
+    CatalogName,
+
+    /// # About
+    /// **Specification:** SQL 1999
+    Chain,
+
+    /// # About
+    /// **Specification:** SQL 1999
+    CharLength,
+
+    /// # About
+    /// **Specification:** SQL 1999
+    CharacterLength,
+
+    /// # About
+    /// **Specification:** SQL 1992
+    CharacterSetCatalog,
+
+    /// # About
+    /// **Specification:** SQL 1992
+    CharacterSetName,
+
+    /// # About
+    /// **Specification:** SQL 1992
+    CharacterSetSchema,
+
+    /// # About
+    /// **Specification:** SQL 1999
+    Checked,
+
+    /// # About
+    /// **Specification:** SQL 1992
+    ClassOrigin,
+
+    /// # About
+    /// **Specification:** SQL 1999
+    Coalesce,
+
+    /// # About
+    /// **Specification:** SQL 1992
+    Cobol,
+
+    /// # About
+    /// **Specification:** SQL 1992
+    CollationCatalog,
+
+    /// # About
+    /// **Specification:** SQL 1992
+    CollationName,
+
+    /// # About
+    /// **Specification:** SQL 1992
+    CollationSchema,
+
+    /// # About
+    /// **Specification:** SQL 1992
+    ColumnName,
+
+    /// # About
+    /// **Specification:** SQL 1992
+    CommandFunction,
+
+    /// # About
+    /// **Specification:** SQL 1999
+    CommandFunctionCode,
+
+    /// # About
+    /// **Specification:** SQL 1992
+    Committed,
+
+    /// # About
+    /// **Specification:** SQL 1992
+    ConditionNumber,
+
+    /// # About
+    /// **Specification:** SQL 1992
+    ConnectionName,
+
+    /// # About
+    /// **Specification:** SQL 1992
+    ConstraintCatalog,
+
+    /// # About
+    /// **Specification:** SQL 1992
+    ConstraintName,
+
+    /// # About
+    /// **Specification:** SQL 1992
+    ConstraintSchema,
+
+    /// # About
+    /// **Specification:** SQL 1999
+    Contains,
+
+    /// # About
+    /// **Specification:** SQL 1999
+    Convert,
+
+    /// # About
+    /// **Specification:** SQL 1999
+    Count,
+
+    /// # About
+    /// **Specification:** SQL 1992
+    CursorName,
+
+
+    /// # About
+    /// **Specification:** SQL 1992
+    DatetimeIntervalCode,
+
+    /// # About
+    /// **Specification:** SQL 1992
+    DatetimeIntervalPrecision,
+
+    /// # About
+    /// **Specification:** SQL 1999
+    Defined,
+
+    /// # About
+    /// **Specification:** SQL 1999
+    Definer,
+
+    /// # About
+    /// **Specification:** SQL 1999
+    Dispatch,
+
+    /// # About
+    /// **Specification:** SQL 1992
+    DynamicFunction,
+
+    /// # About
+    /// **Specification:** SQL 1999
+    DynamicFunctionCode,
+
+
+    /// # About
+    /// **Specification:** SQL 1999
+    Existing,
+
+    /// # About
+    /// **Specification:** SQL 1999
+    Exists,
+
+    /// # About
+    /// **Specification:** SQL 1999
+    Extract,
+
+
+    /// # About
+    /// **Specification:** SQL 1999
+    Final,
+
+    /// # About
+    /// **Specification:** SQL 1992
+    Fortran,
+
+
+    /// # About
+    /// **Specification:** SQL 1999
+    G,
+
+    /// # About
+    /// **Specification:** SQL 1999
+    Generated,
+
+    /// # About
+    /// **Specification:** SQL 1999
+    Granted,
+
+
+    /// # About
+    /// **Specification:** SQL 1999
+    Hierarchy,
+
+    /// # About
+    /// **Specification:** SQL 1999
+    Hold,
+
+
+    /// # About
+    /// **Specification:** SQL 1999
+    Implementation,
+
+    /// # About
+    /// **Specification:** SQL 1999
+    Infix,
+
+    /// # About
+    /// **Specification:** SQL 1999
+    Insensitive,
+
+    /// # About
+    /// **Specification:** SQL 1999
+    Instance,
+
+    /// # About
+    /// **Specification:** SQL 1999
+    Instantiable,
+
+    /// # About
+    /// **Specification:** SQL 1999
+    Invoker,
+
+
+    /// # About
+    /// **Specification:** SQL 1999
+    K,
+
+    /// # About
+    /// **Specification:** SQL 1999
+    KeyMember,
+
+    /// # About
+    /// **Specification:** SQL 1999
+    KeyType,
+
+
+    /// # About
+    /// **Specification:** SQL 1992
+    Length,
+
+    /// # About
+    /// **Specification:** SQL 1999
+    Lower,
+
+
+    /// # About
+    /// **Specification:** SQL 1999
+    M,
+
+    /// # About
+    /// **Specification:** SQL 1999
+    Max,
+
+    /// # About
+    /// **Specification:** SQL 1999
+    Min,
+
+    /// # About
+    /// **Specification:** SQL 1992
+    MessageLength,
+
+    /// # About
+    /// **Specification:** SQL 1992
+    MessageOctetLength,
+
+    /// # About
+    /// **Specification:** SQL 1992
+    MessageText,
+
+    /// # About
+    /// **Specification:** SQL 1999
+    Method,
+
+    /// # About
+    /// **Specification:** SQL 1999
+    Mod,
+
+    /// # About
+    /// **Specification:** SQL 1992
+    More,
+
+    /// # About
+    /// **Specification:** SQL 1992
+    Mumps,
+
+
+    /// # About
+    /// **Specification:** SQL 1992
+    Name,
+
+    /// # About
+    /// **Specification:** SQL 1992
+    Nullable,
+
+    /// # About
+    /// **Specification:** SQL 1992
+    Number,
+
+    /// # About
+    /// **Specification:** SQL 1999
+    Nullif,
+
+
+    /// # About
+    /// **Specification:** SQL 1999
+    OctetLength,
+
+    /// # About
+    /// **Specification:** SQL 1999
+    Options,
+
+    /// # About
+    /// **Specification:** SQL 1999
+    Overlaps,
+
+    /// # About
+    /// **Specification:** SQL 1999
+    Overlay,
+
+    /// # About
+    /// **Specification:** SQL 1999
+    Overriding,
+
+
+    /// # About
+    /// **Specification:** SQL 1992
+    Pascal,
+
+    /// # About
+    /// **Specification:** SQL 1999
+    ParameterMode,
+
+    /// # About
+    /// **Specification:** SQL 1999
+    ParameterName,
+
+    /// # About
+    /// **Specification:** SQL 1999
+    ParameterOrdinalPosition,
+
+    /// # About
+    /// **Specification:** SQL 1999
+    ParameterSpecificCatalog,
+
+    /// # About
+    /// **Specification:** SQL 1999
+    ParameterSpecificName,
+
+    /// # About
+    /// **Specification:** SQL 1999
+    ParameterSpecificSchema,
+
+    /// # About
+    /// **Specification:** SQL 1992
+    Pli,
+
+    /// # About
+    /// **Specification:** SQL 1999
+    Position,
+
+
+    /// # About
+    /// **Specification:** SQL 1992
+    Repeatable,
+
+    /// # About
+    /// **Specification:** SQL 1992
+    ReturnedLength,
+
+    /// # About
+    /// **Specification:** SQL 1992
+    ReturnedOctetLength,
+
+    /// # About
+    /// **Specification:** SQL 1992
+    ReturnedSqlstate,
+
+    /// # About
+    /// **Specification:** SQL 1999
+    RoutineCatalog,
+
+    /// # About
+    /// **Specification:** SQL 1999
+    RoutineName,
+
+    /// # About
+    /// **Specification:** SQL 1999
+    RoutineSchema,
+
+    /// # 1992
+    /// **Specification:** SQL 1999
+    RowCount,
+
+
+    /// # About
+    /// **Specification:** SQL 1992
+    Scale,
+
+    /// # About
+    /// **Specification:** SQL 1992
+    SchemaName,
+
+    /// # About
+    /// **Specification:** SQL 1999
+    Security,
+
+    /// Reserved identifier in `Rust` ^_^
+    /// # About
+    /// **Specification:** SQL 1999
+    #[strum(to_string="SELF")]
+    Self_,
+
+    /// # About
+    /// **Specification:** SQL 1999
+    Sensitive,
+
+    /// # About
+    /// **Specification:** SQL 1992
+    Serializable,
+
+    /// # About
+    /// **Specification:** SQL 1992
+    ServerName,
+
+    /// # About
+    /// **Specification:** SQL 1999
+    Simple,
+
+    /// # About
+    /// **Specification:** SQL 1999
+    Source,
+
+    /// # About
+    /// **Specification:** SQL 1999
+    SpecificName,
+
+    /// # About
+    /// **Specification:** SQL 1999
+    Similar,
+
+    /// # About
+    /// **Specification:** SQL 1999
+    Style,
+
+    /// # About
+    /// **Specification:** SQL 1992
+    SubclassOrigin,
+
+    /// # About
+    /// **Specification:** SQL 1999
+    Sublist,
+
+    /// # About
+    /// **Specification:** SQL 1999
+    Substring,
+
+    /// # About
+    /// **Specification:** SQL 1999
+    Sum,
+
+    /// # About
+    /// **Specification:** SQL 1999
+    Symmetric,
+
+    /// # About
+    /// **Specification:** SQL 1999
+    System,
+
+
+    /// # About
+    /// **Specification:** SQL 1992
+    TableName,
+
+    /// # About
+    /// **Specification:** SQL 1999
+    TransactionsCommitted,
+
+    /// # About
+    /// **Specification:** SQL 1999
+    TransactionsRolledBack,
+
+    /// # About
+    /// **Specification:** SQL 1999
+    TransactionActive,
+
+    /// # About
+    /// **Specification:** SQL 1999
+    Transform,
+
+    /// # About
+    /// **Specification:** SQL 1999
+    Transforms,
+
+    /// # About
+    /// **Specification:** SQL 1999
+    Translate,
+
+    /// # About
+    /// **Specification:** SQL 1999
+    TriggerCatalog,
+
+    /// # About
+    /// **Specification:** SQL 1999
+    TriggerSchema,
+
+    /// # About
+    /// **Specification:** SQL 1999
+    TriggerName,
+
+    /// # About
+    /// **Specification:** SQL 1999
+    Trim,
+
+    /// # About
+    /// **Specification:** SQL 1992
+    Type,
+
+
+    /// # About
+    /// **Specification:** SQL 1992
+    Uncommitted,
+
+    /// # About
+    /// **Specification:** SQL 1992
+    Unnamed,
+
+    /// # About
+    /// **Specification:** SQL 1999
+    Upper,
+
+    /// # About
+    /// **Specification:** SQL 1999
+    UserDefinedTypeCatalog,
+
+    /// # About
+    /// **Specification:** SQL 1999
+    UserDefinedTypeName,
+
+    /// # About
+    /// **Specification:** SQL 1999
+    UserDefinedTypeSchema,
+}
